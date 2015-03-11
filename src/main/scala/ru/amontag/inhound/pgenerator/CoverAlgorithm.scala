8169c362f5b09b774851cc54f9f295c728d53292
montag <montag@yandex-team.ru>
2015-03-11 11:18:44 +0300

    some draft for graph reduction impl

--- src/main/scala/ru/amontag/inhound/pgenerator/CoverAlgorithm.scala
+++ src/main/scala/ru/amontag/inhound/pgenerator/CoverAlgorithm.scala
@@ -1,42 +1,63 @@
 package ru.amontag.inhound.pgenerator
 
-import ru.amontag.inhound.pgenerator.Graph.{Edge, MultiVertex}
+import scala.collection.mutable
 
 /**
  * Created by montag on 10.03.15.
  */
 object CoverAlgorithm {
-    def functor[T](alpha: Double, betta: Double)(vertex: MultiVertex[T]) = {
+    def functor(alpha: Double, betta: Double)(vertex: Vertex) = {
         val SoI: Double = vertex.joinedElements.size
         val CoL: Double = vertex.containsIn.size
 
         Math.pow(SoI, alpha) * Math.pow(CoL, betta)
     }
 
-    def cover[T](sets: Seq[Set[T]], functor: (MultiVertex[T]) => Double): CoverResult[T] = {
+    def cover(sets: Seq[Set[Element]], functor: (Vertex) => Double): CoverResult = {
         val indexedElementsOfSets = sets.zipWithIndex
           .map({ case (set, indexOfLine) => set.map(element => (element, indexOfLine))}).flatten
           .groupBy({ case (element, containsInLine) => element})
           .map({ case (element, containsInLines) => (element, containsInLines.map(_._2).toSet)})
 
-        val vertexes = indexedElementsOfSets.map({ case (element, containsIn) => MultiVertex()(Set(element), containsIn)})
+        val vertexes = indexedElementsOfSets.map({ case (element, containsIn) => Vertex(Set(element), containsIn)})
 
         val edges = vertexes.map(left => (left, vertexes.filter(right => (right.containsIn & left.containsIn).size > 0)))
-          .map({ case (left, neighbors) => neighbors.map(edge(left, _, functor))}).flatten
+          .map({ case (left, neighbors) => neighbors.filter(_ != left).map(new Edge(left, _, functor))})
+          .flatten.toStream.distinct
 
-        edges.foreach(println)
+        implicit val ordering = Ordering.by[Edge, Double](_.weight)
+        val edgesQueue = mutable.PriorityQueue(edges: _*)
+
+        edgesQueue.foreach(println)
         CoverResult(Nil, Nil)
     }
 
-    private def edge[T](left: MultiVertex[T], right: MultiVertex[T], functor: (MultiVertex[T]) => Double) = {
-        val leftWeight = functor(left)
-        val rightWeight = functor(right)
-        val probe = MultiVertex()(left.joinedElements ++ right.joinedElements, left.containsIn & right.containsIn)
-        val probeWeight = functor(probe)
+    case class CoverResult(accepted: List[Set[Element]], rejected: List[Set[Element]])
 
-        Edge(left, right, 2*probeWeight - (leftWeight + rightWeight), probe)
+    case class Element(id: Int)
+
+    case class Vertex(joinedElements: Set[Element],
+                      containsIn: Set[Int])
+
+    class Edge(val left: Vertex, val right: Vertex, f: (Vertex) => Double) {
+        val probe: Vertex = Vertex(left.joinedElements ++ right.joinedElements, left.containsIn & right.containsIn)
+        val weight: Double = 2 * f(probe) - f(left) - f(right)
+
+        def canEqual(other: Any): Boolean = other.isInstanceOf[Edge]
+
+        override def equals(other: Any): Boolean = other match {
+            case that: Edge =>
+                (that canEqual this) && ((left == that.left && right == that.right) || (left == that.right && right == that.left))
+            case _ => false
+        }
+
+        override def hashCode(): Int = left.hashCode() ^ right.hashCode()
+
+        override def toString: String = String.format("left: %s, right: %s, probe: %s, weight: %s",
+            left.toString,
+            right.toString,
+            probe.toString,
+            weight.toString)
     }
-
-    case class CoverResult[T](accepted: List[Set[T]], rejected: List[Set[T]])
 
 }
--- src/test/scala/ru/amontag/inhound/pgenerator/CoverAlgorithm$Test.scala
+++ src/test/scala/ru/amontag/inhound/pgenerator/CoverAlgorithm$Test.scala
@@ -1,19 +1,26 @@
 package ru.amontag.inhound.pgenerator
 
 import org.scalatest.FunSuite
+import ru.amontag.inhound.pgenerator.CoverAlgorithm.{Vertex, Edge, Element}
 
 /**
  * Created by montag on 10.03.15.
  */
 class CoverAlgorithm$Test extends FunSuite {
+    test("Edge equality") {
+        val e1 = new Edge(Vertex(Set(Element(1), Element(2)), Set(0, 1, 2)), Vertex(Set(Element(2), Element(3)), Set(0, 1, 2)), x => 0.0)
+        val e2 = new Edge(Vertex(Set(Element(2), Element(3)), Set(0, 1, 2)), Vertex(Set(Element(1), Element(2)), Set(0, 1, 2)), x => 0.0)
+        assert(e1 == e2)
+    }
+
     test("Just view work of algo") {
-        val sets: Seq[Set[Int]] = Seq(
+        val sets: Seq[Set[Element]] = Seq(
             Set(1, 2, 3, 4),
             Set(2, 3, 4, 5),
             Set(3, 4, 5, 6),
             Set(4, 5, 6, 7)
-        )
+        ).map(_.map(Element))
 
-        CoverAlgorithm.cover(sets, CoverAlgorithm.functor[Int](0.5, 0.5))
+        CoverAlgorithm.cover(sets, CoverAlgorithm.functor(0.5, 0.5))
     }
 }
