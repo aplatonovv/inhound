32308323c1ebcbc3c7d88a56075c80d135b234cf
montag <montag@yandex-team.ru>
2015-03-13 09:09:32 +0300

    complete representation of regexp class set

--- pgenerator/src/main/scala/ru/amontag/inhound/pgenerator/ElementClass.scala
+++ pgenerator/src/main/scala/ru/amontag/inhound/pgenerator/ElementClass.scala
@@ -8,14 +8,43 @@ import scala.util.matching.Regex
 case class ElementClass(id: Int)
 
 abstract class UniverseClassSet {
-    def defineClass(token: String): ElementClass
+    def defineClass(token: String): Option[ElementClass]
+
+    val defaultClass: (ElementClass, String)
+}
+
+object RegexpClassSet {
+    val classes: Map[ElementClass, String] = {
+        val minimalClasses = Map(
+            ElementClass(1) -> "\\d",
+            ElementClass(2) -> "\\s",
+            ElementClass(3) -> "\\w",
+            ElementClass(4) -> "[-+=*\\<>%^]",
+            ElementClass(5) -> "_",
+            ElementClass(6) -> "\\+",
+            ElementClass(7) -> "['\"]",
+            ElementClass(8) -> "[.,;:?!]",
+            ElementClass(9) -> "[#$&@~]",
+            ElementClass(10) -> "[{}\\[\\]()]"
+        )
+        val withNumbers = minimalClasses ++ (1 to 10).map(i => ElementClass(i + minimalClasses.size) -> i.toString)
+        val enAlphabet = "abcdefghijklmnopqrstuvwxyz"
+        val ruAlphabet = "абвгдежзийклмнопрстуфхцчшщъыьэюяё"
+        withNumbers ++
+          (enAlphabet + enAlphabet.toUpperCase + ruAlphabet + ruAlphabet.toUpperCase).toList.zipWithIndex
+            .map({case (letter, num) => ElementClass(num + withNumbers.size) -> letter.toString})
+    }
+
+    val defaultClass = ElementClass(-1) -> "."
 }
 
 class RegexpClassSet extends UniverseClassSet {
-    val classes: Map[Regex, ElementClass] = RegexpPattern.classes.map({case (ec, p) => (p.r, ec)}).toMap
+    val classes: Map[Regex, ElementClass] = RegexpClassSet.classes.map({case (ec, p) => (p.r, ec)}).toMap
 
-    override def defineClass(token: String): ElementClass = {
-        classes.find({case (regexp, ec) => regexp.findFirstIn(token).isDefined}).get._2
+    override def defineClass(token: String): Option[ElementClass] = {
+        classes.find({case (regexp, ec) => regexp.findFirstIn(token).isDefined}).map(_._2)
     }
+
+    override val defaultClass: (ElementClass, String) = RegexpClassSet.defaultClass
 }
 
