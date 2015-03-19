fd8d7d9ef171250805b38296f68275a635602689
montag <montag@yandex-team.ru>
2015-03-19 22:19:03 +0300

    pgenerator splitter to facts

--- pgenerator/src/main/scala/ru/amontag/inhound/pgenerator/PatternGenerator.scala
+++ pgenerator/src/main/scala/ru/amontag/inhound/pgenerator/PatternGenerator.scala
@@ -1,43 +1,13 @@
 package ru.amontag.inhound.pgenerator
 
-import java.util.regex.Pattern
-
-import scala.annotation.tailrec
-
 /**
  * Created by montag on 17.03.15.
  */
 abstract class PatternGenerator {
-    val factMarkupPattern = "<:fact>(.*?)</:fact>".r
-
     def mine(lines: Seq[String]): List[Pattern] = {
-        val (facts, otherLines) = splitTextByFacts(lines)
+        val (facts, otherLines) = MarkupTool.splitTextByFacts(lines)
         mine(facts, otherLines)
     }
 
     protected def mine(facts: Seq[String], otherLines: Seq[String]): List[Pattern]
-
-    def splitTextByFacts(text: Seq[String]): (List[String], List[String]) = {
-        text.map(line => line -> extractFactsFromLine(line))
-          .map(splitLineOnFactsAndOthersSubstrings)
-          .reduce((left, right) => (left._1 ::: right._1) -> (left._2 ::: right._2))
-    }
-
-    def extractFactsFromLine(line: String) = factMarkupPattern.findAllIn(line).toList
-
-    def splitLineOnFactsAndOthersSubstrings(lineAndFacts: (String, List[String])): (List[String], List[String]) = {
-        val (line, facts) = lineAndFacts
-        facts.map(_.replaceAll("</?:fact>", "")) -> splitLineByFacts(line, facts)
-    }
-
-    @tailrec
-    private def splitLineByFacts(line: String, facts: List[String], buffer: List[String] = Nil): List[String] = {
-        facts match {
-            case Nil => (if (line.length > 0) line :: buffer else buffer).reverse
-            case fact :: tail =>
-                val i = line.indexOf(fact)
-                val (beforeFact, afterFact) = (line.take(i), line.drop(i).replaceFirst(Pattern.quote(fact), ""))
-                splitLineByFacts(afterFact, tail, beforeFact :: buffer)
-        }
-    }
 }
