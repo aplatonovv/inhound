3e7a3c3c586c897a801b3f9ceac9a9dace6341aa
montag <montag@yandex-team.ru>
2015-03-19 22:21:28 +0300

    pgenerator splitter to facts

--- pgenerator/src/test/scala/ru/amontag/inhound/pgenerator/CoverAlgorithmBenchmark.scala
+++ pgenerator/src/test/scala/ru/amontag/inhound/pgenerator/CoverAlgorithmBenchmark.scala
@@ -13,13 +13,35 @@ import scala.util.Random
  */
 class CoverAlgorithmBenchmark extends FunSuite {
     test("Test raise classes and sentences from 10 to 10000") {
-        managed(new FileOutputStream("CoverAlgorithmBenchmark_2.tsv"))
+        managed(new FileOutputStream("CoverAlgorithmBenchmark_3.tsv"))
           .flatMap(stream => managed(new PrintWriter(stream)))
           .foreach(writer => {
             writer.println("Count of classes\tCount of sentences\tMeasures")
             val functor = CoverAlgorithm.functor(0.5, 0.5)(_)
 
             for (classCount <- 910 to(1000, 100); sentenceCount <- 4510 to(10000, 500)) {
+                println("Classes: " + classCount + "\tSentences: " + sentenceCount)
+                Random.setSeed(0)
+                val classes = (for (i <- 1 to classCount) yield {
+                    new Element(i) {}
+                }).toArray
+                val sets = (for (i <- 1 to sentenceCount) yield {
+                    randomTake(5, classes)
+                }).toArray
+
+                val durations = Benchmark.repeat[CoverResult](() =>
+                    CoverAlgorithm.cover(sets, functor, false)
+                )(500).map(_.toMillis).mkString("\t")
+
+                writer.println(String.format("%s\t%s\t%s",
+                    classCount.toString,
+                    sentenceCount.toString,
+                    durations))
+                writer.flush()
+            }
+
+            val classCount = 1000
+            for (sentenceCount <- 10 to(10000, 500)) {
                 println("Classes: " + classCount + "\tSentences: " + sentenceCount)
                 Random.setSeed(0)
                 val classes = (for (i <- 1 to classCount) yield {
--- pgenerator/src/test/scala/ru/amontag/inhound/pgenerator/PatternGeneratorTest.scala
+++ pgenerator/src/test/scala/ru/amontag/inhound/pgenerator/PatternGeneratorTest.scala
@@ -5,15 +5,11 @@ import org.scalatest.FunSuite
 /**
  * Created by montag on 19.03.15.
  */
-class PatternGeneratorTest extends FunSuite {
-    val patternGenerator = new PatternGenerator {
-        override protected def mine(facts: Seq[String], otherLines: Seq[String]): List[Pattern] = Nil
-    }
-
+class MarkupToolTest extends FunSuite {
     val line = "kdlfksldkfsdf<:fact>987398371243</:fact>;kdfkselfksdf<:fact>aaaasdasd</:fact>okpoioxicvxcv"
 
     test("split on facts and merge back") {
-        val (facts, other) = patternGenerator.splitLineOnFactsAndOthersSubstrings(line, patternGenerator.extractFactsFromLine(line))
+        val (facts, other) = MarkupTool.splitLineOnFactsAndOthersSubstrings(line, MarkupTool.extractFactsFromLine(line))
         val mergeResult = other.zip(facts.map(f => "<:fact>" + f + "</:fact>")).map(t => t._1 + t._2).mkString("") + other.last
         assertResult(line)(mergeResult)
     }
